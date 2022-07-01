7e081eec323bd8965e1b5348418d9018664a5249
TecJP <tec.joao.freitas@gmail.com>
2022-06-30 23:14:46 -0300

    Increasing the responsiviness and finishing the timer funcionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
-import { Flex, Container, NumberInput, NumberInputField, Heading, Box, Text, ButtonGroup, Button, Spacer } from '@chakra-ui/react'
-import { FormEvent, useEffect, useState } from 'react'
+import { Flex, Container, NumberInput, NumberInputField, Heading, Box, Text, ButtonGroup, Button, Spacer, Center, useToast } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+
+//https://soundbible.com/mp3/Fire_pager-jason-1283464858.mp3
 
 export default function Home() {
+  const [initialTime, setInitialTime] = useState(0);
   const [time, setTime] = useState(0);
   const [active, setActive] = useState(false);
   const [paused, setPaused] = useState(true);
-
+  const toast = useToast();
 
   useEffect(() => {
     let interval: string | number | NodeJS.Timer | undefined;
@@ -14,21 +17,73 @@ export default function Home() {
       interval = setInterval(() => {
         setTime((time) => time - 10);
       }, 10);
-    } else {
-      clearInterval(interval);
     }
+    if (time < 0) {
+      clearInterval(interval);
+      setTime(0);
+      setActive(!active);
+      setPaused(!paused);
+    }
+
+    switch (time) {
+      case 1799990:
+        toast({
+          title: 'Faltam 30 Minutos',
+          status: 'success',
+          duration: 2000,
+        });
+        break;
+      case 1199990:
+        toast({
+          title: 'Faltam 20 Minutos',
+          status: 'success',
+          duration: 2000,
+        });
+        break;
+      case 599990:
+        toast({
+          title: 'Faltam 10 Minutos',
+          status: 'success',
+          duration: 2000,
+        });
+        break;
+      case 299990:
+        toast({
+          title: 'Faltam 5 Minutos',
+          status: 'success',
+          duration: 2000,
+        });
+        break;
+      case 119990:
+        toast({
+          title: 'Faltam 2 minutos',
+          status: 'success',
+          duration: 2000,
+        });
+        break;
+      case 10:
+        toast({
+          title: 'Acabou o tempo',
+          status: 'success',
+          duration: 2000,
+        });
+        break;
+      default:
+        break;
+    }
+
     return () => {
       clearInterval(interval)
     };
-  }, [active, paused]);
-
-  function onChangeTimeInput(event: FormEvent<HTMLInputElement>) {
-    setTime(Number(event.currentTarget.value) * 60000);
-  }
+  }, [active, paused, initialTime, time, toast]);
 
   function handleStart() {
     if (time === 0) {
-      alert('Please, insert a number');
+      toast({
+        title: 'Insira um número',
+        status: 'error',
+        duration: 3000,
+      });
       return;
     } else {
       setActive(true);
@@ -37,8 +92,9 @@ export default function Home() {
   }
 
   function handleReset() {
-    setActive(!active);
     setTime(0);
+    setInitialTime(0);
+    setActive(!active);
   }
 
   function togglePauseButton() {
@@ -47,42 +103,49 @@ export default function Home() {
 
   return (
     <Container minW={'100vw'} minH={'100vh'} p={'0'} bg={'gray.900'}>
-      <Flex gap={'16'} alignItems={'center'} w={'100%'} minHeight={'100%'} direction={'column'}>
-        <Heading color={'#fff'} m={'1rem 0 1rem 0'}>Contador Torneio</Heading>
-        <Spacer />
-        <NumberInput w={{ base: '90%' }} colorScheme={'teal'} color={'#fff'}>
-          <NumberInputField
-            name='time'
-            value={time}
-            onChange={onChangeTimeInput}
-            placeholder='Quantos minutos você deseja?'
-          />
-        </NumberInput>
-        <Spacer />
-        <Box>
-          <Text color={'#fff'} fontSize={'2.4rem'}>
-            {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
-            {("0" + Math.floor((time / 1000) % 60)).slice(-2)}.
-            {("0" + ((time / 10) % 100)).slice(-2)}
-          </Text>
-        </Box>
-        <Spacer />
-        <ButtonGroup w={'90%'}>
-          {
-            active ?
-              <>
-                <Button w='50%' onClick={handleReset}>Reiniciar</Button>
-                <Button w='50%' onClick={togglePauseButton}>
-                  {paused ? 'Continuar' : 'Pausar'}
+      <Center>
+        <Flex gap={'16'} alignItems={'center'} w={'30em'} minHeight={'100%'} direction={'column'}>
+          <Heading color={'#fff'} m={'1rem 0 1rem 0'}>Contador Torneio</Heading>
+          <Spacer />
+          <NumberInput w={{ base: '90%' }} colorScheme={'teal'} color={'#fff'}>
+            <NumberInputField
+              name='time'
+              value={time}
+              disabled={active}
+              onChange={(event) => {
+                setInitialTime(Number(event.target.value) * 60000),
+                  setTime(Number(event.target.value) * 60000)
+              }
+              }
+              placeholder='Quantos minutos você deseja?'
+            />
+          </NumberInput>
+          <Spacer />
+          <Box>
+            <Text color={'#fff'} fontSize={'2.4rem'}>
+              {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
+              {("0" + Math.floor((time / 1000) % 60)).slice(-2)}.
+              {("0" + ((time / 10) % 100)).slice(-2)}
+            </Text>
+          </Box>
+          <Spacer />
+          <ButtonGroup w={'90%'}>
+            {
+              active ?
+                <>
+                  <Button w='50%' onClick={() => handleReset()}>Reiniciar</Button>
+                  <Button w='50%' onClick={togglePauseButton}>
+                    {paused ? 'Continuar' : 'Pausar'}
+                  </Button>
+                </>
+                :
+                <Button w='100%' onClick={handleStart}>
+                  Iniciar
                 </Button>
-              </>
-              :
-              <Button w='100%' onClick={handleStart}>
-                Iniciar
-              </Button>
-          }
-        </ButtonGroup>
-      </Flex>
+            }
+          </ButtonGroup>
+        </Flex>
+      </Center>
     </Container>
   )
 }
